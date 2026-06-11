b737bf36be7723eb922cfdb0a6c5de1daacc94a0
Pallavi Baske <104455586+pallavibaske@users.noreply.github.com>
2026-06-11 10:20:39 +0530

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,10 @@
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+highest = max(numbers)
+lowest = min(numbers)
+average = sum(numbers) / len(numbers)
+
+print("Numbers:", numbers)
+print("Highest:", highest)
+print("Lowest:", lowest)
+print("Average:", average)
